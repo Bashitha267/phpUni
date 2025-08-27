dbe88aa9676cfafa7f555f48ec33d88ce4b8f8cd
= <DCSUser@cn.lan>
2025-08-27 15:06:26 +0530

    added first webapp

--- webapp/login.php
+++ webapp/login.php
@@ -5,14 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
-<body>
-   <div class="login-title" style="text-align: center;">Login</div>
-   <form action="login.php" method="post">
-    <div class="login-section" style="display: flex;justify-content: center;flex-direction: column;">
+<body style="display:grid;place-items: center;">
+   <div class="login-title" style="font-size:larger; font-weight: 600; margin-bottom: 15px;">Login</div>
+   <form action="login.php" method="post" style="border:solid;border-color: black; padding: 15px;">
+    <div class="login-section" style="display: flex;justify-content: center;flex-direction: column; height: 100%; width:80%;">
     <div> <label>Username:</label>
     <input type="text"></div>
    <div>  <label>Password:</label>
     <input type="password">
+    </div>
+    <div class="" style="margin-top: 10px;">
+        <input type="submit" value="Login">
     </div>
     </div>
   
