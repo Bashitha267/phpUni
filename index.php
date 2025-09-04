0adc56ba8c32a527c4522b74814ebf7d72106880
DCS User <DCSUser@cn.lan>
2025-09-04 16:07:36 +0530

    edited

--- index.php
+++ index.php
@@ -1,40 +1,12 @@
-<?php
-$array=[
-    ["name"=>"Nimesh","Age"=>25],
-    ["name"=>"Bashitha","Age"=>25],
-    ["name"=>"Kamal","Age"=>75],
-
-];
-// echo"$array[b]";
-$fruits=[
-    "Apple","Banana","Cherry"
-];
-
-?>
 <html>
     <head>
-        
-    <title>this is my first</title>
-    <link rel="stylesheet" href="./mycss.css">  
-</head>
-<body>
-<div >
-    <!-- <ul>
-    <?php 
-    foreach($array as $key=>$value){
-        echo"<li class=\"name\">Name:{$value['name']}, Age:{$value['Age']}
-        </li>";
-    }
-
-?>
-    </ul> -->
-<?php
-$array12=array_unshift($fruits,1);
-
-print_r($fruits);
-?>
-
-</div>  
-</body>
-  
-</html>
+        <title>My first PHP Website</title>
+    </head>
+    <body>
+        <?php
+            echo "<p>Hello World!</p>";
+        ?>
+        <a href="login.php"> Click here to login </br>
+        <a href="register.php"> Click here to register 
+    </body>
+</html> 
